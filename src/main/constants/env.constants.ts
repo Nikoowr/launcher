2469da3b2c637e68728a1068071bdab675820b41
Gabriel Ribeiro <ribeirogabx@gmail.com>
2023-11-20 08:46:06 -0300

    feat: electron + react boilerplate

---
+++ src/main/constants/env.constants.ts
@@ -0,0 +1,2 @@
+export const NODE_ENV = process.env.NODE_ENV ?? 'development';
+export const ELECTRON_PORT = process.env.PORT ?? 1212;
---
+++ src/main/preload.ts
@@ -0,0 +1,30 @@
+import { IpcRendererEvent, contextBridge, ipcRenderer } from 'electron';
+
+export type Channels = 'ipc-example';
+
+const electronHandler = {
+  ipcRenderer: {
+    sendMessage(channel: Channels, ...args: unknown[]) {
+      ipcRenderer.send(channel, ...args);
+    },
+
+    on(channel: Channels, func: (...args: unknown[]) => void) {
+      const subscription = (_event: IpcRendererEvent, ...args: unknown[]) =>
+        func(...args);
+      ipcRenderer.on(channel, subscription);
+
+      return () => {
+        ipcRenderer.removeListener(channel, subscription);
+      };
+    },
+
+    once(channel: Channels, func: (...args: unknown[]) => void) {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      ipcRenderer.once(channel, (_event: any, ...args: any) => func(...args));
+    },
+  },
+};
+
+contextBridge.exposeInMainWorld('electron', electronHandler);
+
+export type ElectronHandler = typeof electronHandler;
