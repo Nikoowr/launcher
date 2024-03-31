d464dc13a7ab1bb53b0c05b09caf2de9d074437c
Gabriel Ribeiro <ribeirogabx@gmail.com>
2024-03-31 07:16:09 -0300

    feat: update

--- src/renderer/components/custom/sign-in-side-background.tsx
+++ src/renderer/components/custom/sign-in-side-background.tsx
@@ -2,10 +2,10 @@ import SideImageBackground from '../../../../assets/sign-in-side.jpg';
 
 export const SignInSideBackground = () => {
   return (
-    <div className="absolute inset-0 -z-10 bg-zinc-900 brightness-75">
+    <div className="absolute inset-0 -z-10 bg-zinc-900 brightness-50">
       <img
         src={SideImageBackground}
-        className="size-full object-cover blur-sm"
+        className="size-full object-cover"
         alt="Side Image GF Chaos"
       />
     </div>
